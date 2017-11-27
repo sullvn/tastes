26d7a2bcd87f474f812f3a1a13bae7ee2f0a090c
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2017-11-26 23:28:24 -0600

    Move all code tools to ingesting JS
    Instead of sometimes reaching directly for the original Typescript files.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react'
 
 
-const req = require.context( '../stories', true, /\.stories\.tsx$/ )
+const req = require.context( '../stories', true, /\.stories\.jsx$/ )
 
 function loadStories() {
   req.keys().forEach( fileName => req( fileName ))
