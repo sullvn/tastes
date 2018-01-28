6dfad37a57a07b06d8ad8a857943dad1c77ffede
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2018-01-27 20:39:37 -0800

    Update `Arbitrary` documentation

--- src/types.ts
+++ src/types.ts
@@ -1,43 +1,39 @@
 /**
  * ## Arbitrary
- * 
+ *
  * A function which generates an arbitrary instance of a data type given a number.
  * Each number is mapped to a unique instance.
- * 
+ *
  * It has the following guarantees:
- * 
+ *
  * #### Unique
- * 
+ *
  * For any numbers `m` and `n`, `arbitrary(m) !== arbitrary(n)`.
- * 
+ *
  * #### Locality
- * 
+ *
  * Closer numbers return more similar instances. In other words, the output
  * domain has a total ordering:
- * 
- *     arbitrary(n) < arbitrary(n+1)
- * 
+ *
+ *     arbitrary(m) <= arbitrary(n) when m <= n
+ *
  */
 export interface Arbitrary<T> {
   (id: number): T
 }
 
-
 /**
  * ## ArbitraryType
- * 
+ *
  * Any function which creates arbitrary generator functions.
  */
 export interface ArbitraryType<T> {
   (...args: any[]): Arbitrary<T>
 }
 
-
 /**
  * ## Arbitrary Values
  *
  * Any object which exclusively has arbitrary generators for values.
  */
-export type ArbitraryValues<T> = {
-  [K in keyof T]: Arbitrary<T[K]>
-}
+export type ArbitraryValues<T> = { [K in keyof T]: Arbitrary<T[K]> }
