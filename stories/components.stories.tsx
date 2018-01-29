4d8017807a2309f1579a3699b621f619aeceb9b3
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2018-01-28 21:28:13 -0800

    Font pairing examples

--- stories/components.stories.tsx
+++ stories/components.stories.tsx
@@ -6,7 +6,7 @@ import Samples from '../src/components/Samples'
 import Sampler from '../src/components/Sampler'
 import Card from './components/Card'
 import GradientSwatch from './components/GradientSwatch'
-import { array, string, record, number, Arbitrary } from '../src'
+import { array, string, record, number, sample, Arbitrary } from '../src'
 
 storiesOf('Samples', module)
   .add('of 500 gradients', () => (
@@ -19,6 +19,11 @@ storiesOf('Samples', module)
       {cardExample.render}
     </Samples>
   ))
+  .add('of 200 font pairs', () => (
+    <Samples generator={fontPairExample.generator} count={200}>
+      {fontPairExample.render}
+    </Samples>
+  ))
 
 storiesOf('Sampler', module)
   .add('of gradients', () => (
@@ -28,6 +33,11 @@ storiesOf('Sampler', module)
   ))
   .add('of cards', () => (
     <Sampler generator={cardExample.generator}>{cardExample.render}</Sampler>
+  ))
+  .add('of font pairs', () => (
+    <Sampler generator={fontPairExample.generator}>
+      {fontPairExample.render}
+    </Sampler>
   ))
 
 interface ArbitraryExample<T> {
@@ -96,6 +106,63 @@ const cardExample: ArbitraryExample<Card> = {
         <p>{text}</p>
         <ul>{list.map((x: string, i: number) => <li key={i}>{x}</li>)}</ul>
       </Card>
+    )
+  },
+}
+
+type Font = Pick<
+  React.CSSProperties,
+  'fontWeight' | 'fontFamily' | 'fontSize' | 'fontStyle'
+>
+
+interface FontPair {
+  header: Font
+  body: Font
+}
+
+const fontPairExample: ArbitraryExample<FontPair> = {
+  generator: (function() {
+    const arbitraryFont = ({ min, max }: { min: number; max: number }) =>
+      record({
+        fontWeight: sample(['normal', 'bold']) as Arbitrary<'normal' | 'bold'>,
+        fontFamily: sample([
+          'Georgia',
+          '"Palatino Linotype"',
+          '"Times New Roman"',
+          'Arial',
+          '"Comic Sans MS"',
+          'Impact',
+          'Verdana',
+          'Courier New',
+        ]),
+        fontSize: compose(n => `${n}px`, number({ min, max })),
+        fontStyle: sample(['normal', 'italic']) as Arbitrary<
+          'normal' | 'italic'
+        >,
+      })
+
+    return record({
+      header: arbitraryFont({ min: 16, max: 40 }),
+      body: arbitraryFont({ min: 10, max: 18 }),
+    })
+  })(),
+  render({ header, body }) {
+    return (
+      <section>
+        <h4 style={header}>Ambystoma Mexicanum</h4>
+        <p style={body}>
+          The axolotl (/ˈæksəlɒtəl/, from Classical Nahuatl: āxōlōtl
+          [aːˈʃoːloːtɬ] (About this sound listen)) also known as a Mexican
+          salamander (Ambystoma mexicanum) or a Mexican walking fish, is a
+          neotenic salamander, closely related to the tiger salamander.[2][3]
+          Although the axolotl is colloquially known as a "walking fish", it is
+          not a fish, but an amphibian. The species originates from numerous
+          lakes, such as Lake Xochimilco underlying Mexico City.[4] Axolotls are
+          unusual among amphibians in that they reach adulthood without
+          undergoing metamorphosis. Instead of developing lungs and taking to
+          the land, adults remain aquatic and gilled.
+        </p>
+      </section>
     )
   },
 }
